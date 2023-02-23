6537361f6d93ac7de8c9a9a87916114e661ba0c5
pesa2001 <Sairaj.Perneedi.consultant@nielseniq.com>
2023-02-23 15:34:21 +0530

    Initial commit

---
+++ kafka-integration/src/main/java/com/example/kafka/integration/kafka/KafkaProducer.java
@@ -0,0 +1,20 @@
+package com.example.kafka.integration.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        LOGGER.info(String.format("Message sent -> %s", message));
+        kafkaTemplate.send("coding", message);
+    }
+}
---
+++ kafka-integration/src/main/java/com/example/kafka/integration/kafka/KafkaTopicConfig.java
@@ -0,0 +1,15 @@
+package com.example.kafka.integration.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic practiceTopic(){
+        return TopicBuilder.name("coding").build();
+    }
+}
